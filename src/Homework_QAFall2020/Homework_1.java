d9cba052fc453ffa302cbadee9b59fc2d0bf6ace
Iftaqar Sayed <iftaqarsayed@Iftaqars-MBP.home>
2020-04-29 02:10:19 -0400

    First homework

---
+++ src/Homework_QAFall2020/Homework_1.java
@@ -0,0 +1,23 @@
+package Homework_QAFall2020;
+
+public class Homework_1 {
+
+    public static void main(String[] args) {
+
+        int d = 5;
+        System.out.println(d++); // 5
+        System.out.println(d++); // 6
+        System.out.println(++d); // 8
+        System.out.println(d);   // 8
+        System.out.println(--d); // 7
+        System.out.println(d);   // 7
+        System.out.println(d++);  // 7
+
+        System.out.println(d);     // 8
+        System.out.println(d--);  //  8
+        System.out.println(d++);  //  7
+        System.out.println(d++);  // 8
+        System.out.println(d);    // 9
+    }
+
+}
---
+++ src/Homework_QAFall2020/Homework_2.java
@@ -0,0 +1,28 @@
+package Homework_QAFall2020;
+
+public class Homework_2 {
+    public static void main(String[] args) {
+
+        int numberOfHoursInADay = 24;
+        System.out.println("number of hours in a day is " + numberOfHoursInADay);
+        int numberOfMaxDaysInAYear = 366;
+        System.out.println("number of max no. of days in a years is " + numberOfMaxDaysInAYear);
+        int totalNumberOfEmployees = 3000;
+        System.out.println("total number of employees in an organisation is " + totalNumberOfEmployees);
+        int populationInACountry = 20000000;
+        System.out.println("population in a country is " + populationInACountry);
+        double interestRate = 3.7;
+        System.out.println("interest rate is " + interestRate + "%");
+        double balanceInBankAccount = 25687.89;
+        System.out.println("balance in a bank account is " + balanceInBankAccount + " " + "dollar");
+        boolean doesTheSunRiseFromTheWest = true;
+        System.out.println("does the sun rise from the west? " + doesTheSunRiseFromTheWest);
+        String initials = "IS";
+        System.out.println("initials of my name is " + initials);
+        String myName = "Iftaqar Sayed";
+        System.out.println("My name is " + myName);
+
+
+    }
+
+}
