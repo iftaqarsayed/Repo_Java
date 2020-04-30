34279b007a37d0ace1659d82e3d22049fe701447
Iftaqar Sayed <iftaqarsayed@Iftaqars-MBP.home>
2020-04-30 16:34:45 -0400

    First homework

--- src/Homework_QAFall2020/Homework_3.java
+++ src/Homework_QAFall2020/Homework_3.java
@@ -5,19 +5,19 @@ public class Homework_3 {
 
         double cTemperature = 16;
         double fTemperature = 9/5.0 * (cTemperature) + 32;
-        System.out.println(fTemperature);
+        System.out.println("Temperature in fahrenheit is " + fTemperature);
         double kTemperature2 = 200;
         double fTemparature2 = 9/5.0 * (kTemperature2 - 273) + 32;
-        System.out.println(fTemparature2);
+        System.out.println("Temperature in fahrenheit is " + fTemparature2);
         double cTemperature3 = 30.5;
         double kTemperature3 = cTemperature3 + 273;
-        System.out.println(kTemperature3);
+        System.out.println("Temperature in kelvin is " + kTemperature3);
         double kTemperature4 = 303.50;
         double cTemperature4 = kTemperature4 - 273;
-        System.out.println(cTemperature4);
+        System.out.println("Temperature in celsius is " + cTemperature4);
         double fTemperature5 = 90;
         double kTemperature5 = 5/9.0 * (fTemperature5 - 32) + 273;
-        System.out.println(kTemperature5);
+        System.out.println("Temperature in kelvin is " + kTemperature5);
         // values are a little different because in the website they use 273.15 instead of 273
 
     }
