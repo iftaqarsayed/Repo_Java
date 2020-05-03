e3aa6ea3587ae0b7fed8a363f2318a1fe2185a76
Iftaqar Sayed <iftaqarsayed@Iftaqars-MBP.home>
2020-05-03 16:42:19 -0400

    First homework

--- src/Homework_QAFall2020/HW_04_30_3.java
+++ src/Homework_QAFall2020/HW_04_30_3.java
@@ -1,5 +1,7 @@
 package Homework_QAFall2020;
 
+import java.util.Arrays;
+
 public class HW_04_30_3 {
     public static void main(String[] args) {
         String firstName = "Iftaqar";
@@ -9,6 +11,12 @@ public class HW_04_30_3 {
 
         System.out.println("the length of my first name is: " + length);
 
+       String[] arrayFirstName = firstName.split("");
+        System.out.println(Arrays.toString(arrayFirstName));
+
+        int lengthOfArray = arrayFirstName.length;
+
+        System.out.println("length of array is : " + lengthOfArray);
 
     }
 }
