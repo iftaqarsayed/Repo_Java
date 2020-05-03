3edf0480b20f674ab53d34a870eb382907fd2945
Iftaqar Sayed <iftaqarsayed@Iftaqars-MBP.home>
2020-05-03 01:48:25 -0400

    First homework

---
+++ src/Homework_QAFall2020/HW_04_30_3.java
@@ -0,0 +1,14 @@
+package Homework_QAFall2020;
+
+public class HW_04_30_3 {
+    public static void main(String[] args) {
+        String firstName = "Iftaqar";
+
+        int indexOfr = firstName.indexOf('r');
+        int length = indexOfr + 1;
+
+        System.out.println("the length of my first name is: " + length);
+
+
+    }
+}
